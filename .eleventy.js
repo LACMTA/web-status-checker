c0f558ece28836836077ef45ea118309c55a39ff
albertkun <albertk@gmx.com>
2022-08-25 16:03:58 -0700

    feat: update github actions

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy("src/css");
 	eleventyConfig.addPassthroughCopy("src/js");
+	eleventyConfig.addPassthroughCopy("data");
 
 	return {
 		pathPrefix: "/11ty-web-template/",
